da4ded7c10ccd3ccaf447db1f8a4d2fee1371dff
hkareem <hiramkareem_93@hotmail.com>
2013-07-10 13:06:06 -0700

    practicas 10 de julio

---
+++ tooogle2/timers.h
@@ -0,0 +1,12 @@
+#ifndef __TIMERS__
+#define __TIMERS__
+
+    void timers_init(void);
+    void timers_set_time(int8 timer, int8 time);
+    void timers_get_time(int8 timer);
+    
+    
+
+#endif
+
+
